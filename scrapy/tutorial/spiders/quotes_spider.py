85121abd0c4074cc0ebc8082597d2ee0bf7f11c6
Wilmar Magbanua <wilmarmagbanuadev@gmail.com>
2026-07-23 10:50:38 +0800

    remove /

--- scrapy/tutorial/spiders/quotes_spider.py
+++ scrapy/tutorial/spiders/quotes_spider.py
@@ -76,11 +76,11 @@ class QuotesSpider(scrapy.Spider):
 
     async def start(self):
         urls = [
-            "https://www.facebook.com/negrospowerph/",
-            "https://www.facebook.com/murciawaterdistrict/",
-            "https://www.facebook.com/NGCPph/",
-            "https://www.facebook.com/PAGASA.DOST.GOV.PH/",
-            "https://www.facebook.com/profile.php?id=61573925486634/",
+            "https://www.facebook.com/negrospowerph",
+            "https://www.facebook.com/murciawaterdistrict",
+            "https://www.facebook.com/NGCPph",
+            "https://www.facebook.com/PAGASA.DOST.GOV.PH",
+            "https://www.facebook.com/profile.php?id=61573925486634",
         ]
 
         for url in urls:
